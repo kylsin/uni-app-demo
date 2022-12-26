9e9f4d3541355d3c04fb2ef6ae6d17ed43b9e6ab
魏博康 <158310226@qq.com>
2022-12-26 18:16:37 +0800

    feat: init

---
+++ src/pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <status-bar />
+  <image :src="logo" mode="scaleToFill" />
+</template>
+
+<script lang="ts" setup>
+import statusBar from '@/components/status-bar.vue'
+const logo = `../static/logo/${import.meta.env.VITE_APP_LOGO}`
+</script>
+<style scoped lang="scss"></style>
---
+++ src/types/env.d.ts
@@ -0,0 +1,4 @@
+interface ImportMetaEnv {
+   readonly VITE_APP_LOGO: string
+   readonly VITE_APP_TITLE: string
+}
